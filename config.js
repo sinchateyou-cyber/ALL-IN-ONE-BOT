11c534de72184de63688dcd5c23799995badd75d
sinchateyou-cyber <sinchateyou@gmail.com>
2026-08-06 12:43:12 -0300

    Actualizar config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const { ActivityType } = require('discord.js');
 
 module.exports = {
-  ownerId: '1004206704994566164',
+  ownerId: '831357614267957279',
   status: {
     rotateDefault: [
       { name: 'Netflix', type: ActivityType.Watching },
